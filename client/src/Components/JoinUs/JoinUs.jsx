1ff42bd1a14a16c40496ed62b7224a5ce1aa4066
nakuldevkhajuria <nakuldevkhajuria@gmail.com>
2023-06-04 18:08:18 +0530

    added more components with css

--- client/src/Components/JoinUs/JoinUs.jsx
+++ client/src/Components/JoinUs/JoinUs.jsx
@@ -8,9 +8,81 @@ const JoinUs = () => {
                     <span>
                         <span> Hear more stories from our alumni </span>
                     </span>
-                 
+
                 </button>
             </div>
+            <div className='fang-section'>
+                <div className='fang-container'>
+                    <div className='fang-content'>
+                        <div className='fang-content-box1'>
+                            <div >Why Join Us?</div>
+                            <div > Great students deserve the best jobs </div>
+                            <div> Coding Ninjas Makes it Happen... </div>
+                        </div>
+                        <div className='fang-content-box2'>
+                            <div style={{ display: 'flex' }}>
+                                <div className='fang-content-item1'>
+                                    <img alt='hex' src="https://files.codingninjas.in/hex-9602.svg" />
+                                    <div>
+                                        <div> 150+ </div>
+                                        <div> Students working in <br /> FAANG </div>
+                                    </div>
+                                </div>
+                                <div className='fang-content-item1'>
+                                    <img alt='hex' src="https://files.codingninjas.in/hex-9602.svg" />
+                                    <div>
+                                        <div> 10</div>
+                                        <div> Students started their <br />  own companies </div>
+                                    </div>
+                                </div>
+
+                            </div>
+                            <div className='fang-two-boxes' style={{ display: 'flex' }}>
+                                <div className='fang-content-item1'>
+                                    <img alt='hex' src="https://files.codingninjas.in/hex-9602.svg" />
+                                    <div>
+                                        <div> 10</div>
+                                        <div> Students started their <br />  own companies </div>
+                                    </div>
+                                </div>
+                                <div className='fang-content-item1'>
+                                    <img alt='hex' src="https://files.codingninjas.in/hex-9602.svg" />
+                                    <div>
+                                        <div> 10</div>
+                                        <div> Students started their <br />  own companies </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+            <div className='companies-section'>
+                <div className="companies-container">
+                    <div className="companies-content">
+                        <h2 _ngcontent-serverapp-c107="" class="top-text"> Our students are placed at </h2>
+
+                        <div className='companies-name'>
+                            <img  loading="lazy" alt="Google" srcset="" src="https://files.codingninjas.in/google-9542.svg" class="image-loaded"/>
+                            <img loading="lazy" alt="Microsoft" srcset="" src="https://files.codingninjas.in/microsoft-9541.svg" class="image-loaded" />
+                            <img loading="lazy" alt="Adobe" srcset="" src="https://files.codingninjas.in/adobe-9540.svg" class="image-loaded" />
+                            <img loading="lazy" alt="Walmart" srcset="" src="https://files.codingninjas.in/walmart-9539.svg" class="image-loaded" />
+                            <img loading="lazy" alt="Amazon" srcset="" src="https://files.codingninjas.in/amazon-9538.svg" class="image-loaded" />
+                            <img loading="lazy" alt="Oyo" srcset="" src="https://files.codingninjas.in/oyo-9537.svg" class="image-loaded" />
+                            <img loading="lazy" alt="Flipkart" srcset="" src="https://files.codingninjas.in/flipkart-9536.svg" class="image-loaded" />
+                            <img loading="lazy" alt="MorganStanley" srcset="" src="https://files.codingninjas.in/morganstanley-9535.svg" class="image-loaded" />
+                            <img loading="lazy" alt="Samsung" srcset="" src="https://files.codingninjas.in/samsung-9534.svg" class="image-loaded" />
+                            <img loading="lazy" alt="Expedia" srcset="" src="https://files.codingninjas.in/expedia-9533.svg" class="image-loaded" />
+                            <img loading="lazy" alt="Facebook" srcset="" src="https://files.codingninjas.in/facebook-9532.svg" class="image-loaded" />
+
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+
         </div>
     )
 }
