b8169c6e88be29f62a259c48b804a8a9f0561eba
nakuldevkhajuria <nakuldevkhajuria@gmail.com>
2023-06-07 23:01:19 +0530

    started working on workExperience component

--- client/src/Components/WorkExperience/WorkExperience.jsx
+++ client/src/Components/WorkExperience/WorkExperience.jsx
@@ -4,6 +4,9 @@ const WorkExperience = () => {
   return (
     <div className='work-section'>
         <div className="work-container">
+          <div className="work-content">
+            
+          </div>
             
         </div>
     </div>
