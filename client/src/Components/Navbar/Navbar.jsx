924aa730e48b26186a42edd042f157ecc9190c3f
nakuldevkhajuria <nakuldevkhajuria@gmail.com>
2023-06-13 22:56:20 +0530

    did some changes with the side bar. which becomes responsive on decreasing its width

--- client/src/Components/Navbar/Navbar.jsx
+++ client/src/Components/Navbar/Navbar.jsx
@@ -1,10 +1,11 @@
 
 import './Navbar.css'
 const Navbar = () => {
- 
+
     return (
         <div className='navbar-section'>
             <div className="navbar-container">
+
                 <div className="navbar-content">
                     <div className="main-logo">
                         <a href="/">
@@ -25,21 +26,44 @@ const Navbar = () => {
                             className="nav-image  ng-lazyloaded"
                             src="https://files.codingninjas.in/careercamp-12614.svg" />
                     </div>
-                    <a href="#work-box">
-                    <button className='login-button'>
-                         <span>Login </span>
-                       </button>
-                       </a>
-                  
-              <a href="#work-box">
-                       <button className='enrol-button'> 
-                         <span> Enrol Now </span>
-                       </button>
-                       </a>
-                  
-                          
-                     
+                    <div id='login-enroll'>
+                        <a href="#work-box">
+                            <button className='login-button'>
+                                <span>Login </span>
+                            </button>
+                        </a>
+
+                        <a href="#work-box">
+                            <button className='enrol-button'>
+                                <span> Enroll Now </span>
+                            </button>
+                        </a>
+                    </div>
+
+
+
                 </div>
+
+                <div className='small-navbar-content'>
+                    <button>
+                        click to open
+                    </button>
+                    <div className="main-logo">
+                        <a href="/">
+                            <img
+                                alt="Coding Ninjas"
+                                src="https://files.codingninjas.in/logo_variants-white-25005.png" />
+                        </a>
+                    </div>
+                    <div>
+                        <a href="#work-box">
+                            <button className='login-button'>
+                                <span>Login </span>
+                            </button>
+                        </a>
+                    </div>
+                </div>
+
             </div>
         </div>
     )
